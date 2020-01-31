abcedbe9f4ab89bc587d0ac8d605a380f0286db4
Wolfgang Fahl <wf@bitplan.com>
2020-01-31 14:55:57 +0100

    adds setup and fixes conf for Readthedocs

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='vertx-eventbus-python',
+    version='3.8.5',
+
+    packages=['Vertx',],
+    license='Apache Software License 2.0',
+    long_description=open('README').read(),
+)
